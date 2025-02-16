13f294513aa06ce47bd137bad79c59585f3715c5
TylerBloom <tylerbloom2222@gmail.com>
2025-02-16 20:51:57 +0000

    Working on BullyGB tests

---
+++ spirit/tests/age.rs
@@ -0,0 +1,39 @@
+use spirit::{
+    lookup::{HalfRegister, Instruction, LoadOp, RegOrPointer},
+    Gameboy,
+};
+
+const REG_B: RegOrPointer = RegOrPointer::Reg(HalfRegister::B);
+const END_OP: Instruction = Instruction::Load(LoadOp::Basic {
+    dest: REG_B,
+    src: REG_B,
+});
+
+fn run(cart: &[u8]) {
+    let mut gb = Gameboy::new(cart).complete();
+    while !matches!(gb.read_op(), END_OP) {
+        gb.step().complete();
+    }
+    const REGS: [(HalfRegister, u8); 6] = [
+        (HalfRegister::B, 3),
+        (HalfRegister::C, 5),
+        (HalfRegister::D, 8),
+        (HalfRegister::E, 13),
+        (HalfRegister::H, 21),
+        (HalfRegister::L, 34),
+    ];
+    let cpu = gb.cpu();
+    for (reg, value) in REGS {
+        assert_eq!(cpu[reg].0, value, "{cpu:?}");
+    }
+}
+
+#[test]
+fn vram_read() {
+    run(include_bytes!("roms/age/vram/vram-read-cgbBCE.gb"));
+}
+
+#[test]
+fn ei_halt() {
+    run(include_bytes!("roms/age/halt/ei-halt-dmgC-cgbBCE.gb"));
+}
