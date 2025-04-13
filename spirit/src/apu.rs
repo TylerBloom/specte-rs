23c442ac192a45bd3b377afc9bb82f415d414060
TylerBloom <tylerbloom2222@gmail.com>
2025-04-13 16:22:53 -0400

    Initial draft of APU and audio registers

--- spirit/src/apu.rs
+++ spirit/src/apu.rs
@@ -3,6 +3,9 @@
 pub struct Apu {
 }
 
+pub struct Envelope {
+}
+
 pub enum PulseWidthSetting {
 }
 
@@ -16,9 +19,14 @@ impl PulseWidthChannel {
 }
 
 pub struct WaveChannel {
+    env: Envelope,
 }
 
 impl WaveChannel {
+    /// The channel can be enabled to automatically turn off. When it does, a counter is ticked up
+    /// from a starting value (set by a register) until it reaches this value.
+    const LENGTH: usize = 64;
+
     fn trigger(&mut self) {
         todo!()
     }
@@ -28,6 +36,10 @@ pub struct NoiseChannel {
 }
 
 impl NoiseChannel {
+    /// The channel can be enabled to automatically turn off. When it does, a counter is ticked up
+    /// from a starting value (set by a register) until it reaches this value.
+    const LENGTH: usize = 256;
+
     fn trigger(&mut self) {
         todo!()
     }
